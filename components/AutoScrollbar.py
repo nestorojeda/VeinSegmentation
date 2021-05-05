fa2dd842ed4d3a107fa85c8d1644f996cf9ed975
Néstor Ojeda González <nestorojedagonzalez@gmail.com>
2021-05-05 17:37:18 +0100

    ventana con zoom, abrir archivo y mover

---
+++ components/AutoScrollbar.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Advanced zoom example. Like in Google Maps.
+# It zooms only a tile, but not the whole image. So the zoomed tile occupies
+# constant memory and not crams it with a huge resized image for the large zooms.
+import random
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+
+# https://stackoverflow.com/questions/41656176/tkinter-canvas-zoom-move-pan
+class AutoScrollbar(ttk.Scrollbar):
+    """ A scrollbar that hides itself if it's not needed.
+        Works only if you use the grid geometry manager """
+
+    def set(self, lo, hi):
+        if float(lo) <= 0.0 and float(hi) >= 1.0:
+            self.grid_remove()
+        else:
+            self.grid()
+            ttk.Scrollbar.set(self, lo, hi)
+
+    def pack(self, **kw):
+        raise tk.TclError('Cannot use pack with this widget')
+
+    def place(self, **kw):
+        raise tk.TclError('Cannot use place with this widget')
--- gui.py
+++ gui.py
@@ -1,12 +1,12 @@
-import tkinter
 import sys
-import PIL.Image
-import PIL.ImageTk
-import cv2
+import tkinter as tk
+from tkinter import Tk, Frame, messagebox, ttk, Menu
+from tkinter import filedialog as fd
 
-from tkinter import Tk, Frame, Menu, messagebox
-from VeinSegmentation.enhance import enhance_medical_image
-from components.ResizableCanvas import ResizingCanvas
+from PIL import Image
+from PIL import ImageTk
+
+from components.AutoScrollbar import AutoScrollbar
 
 y = 1300  # donde empieza el corte en y
 x = 1600  # donde empieza el corte en x
@@ -16,54 +16,157 @@ w = 600  # tamaño del corte en y
 
 # https://zetcode.com/tkinter/menustoolbars/
 # https://solarianprogrammer.com/2018/04/20/python-opencv-show-image-tkinter-window/
+# https://www.semicolonworld.com/question/55637/how-to-get-tkinter-canvas-to-dynamically-resize-to-window-width
 
 class App(Frame):
+    ''' Advanced zoom of the image '''
 
-    def __init__(self, window, image_path="imagenes_orginales/Caso A BN.png"):
-        super().__init__(window)
-        self._job = None
-        self.window = window
-        self.window.protocol("WM_DELETE_WINDOW", self.onExit)
-        self.initUI(image_path)
+    def __init__(self, mainframe, path="imagenes_orginales/Caso A BN.png", **kw):
+        ''' Initialize the main Frame '''
+        ttk.Frame.__init__(self, master=mainframe)
+        super().__init__(**kw)
+        self.master.title('Segmentación de venas')
+        self.master.protocol("WM_DELETE_WINDOW", self.onExit)
+        self.filename = path
+        self.initUiComponents()
 
-    def initUI(self, image_path):
+    def initUiComponents(self):
+        # Vertical and horizontal scrollbars for canvas
+        vbar = AutoScrollbar(self.master, orient='vertical')
+        hbar = AutoScrollbar(self.master, orient='horizontal')
+        vbar.grid(row=0, column=1, sticky='ns')
+        hbar.grid(row=1, column=0, sticky='we')
 
+        # Menu bar
         menubar = Menu(self.master)
         self.master.config(menu=menubar)
-
         fileMenu = Menu(menubar)
-        fileMenu.add_command(label="Exit", command=self.onExit)
-        menubar.add_cascade(label="File", menu=fileMenu)
+        fileMenu.add_command(label="Abrir Archivo", command=self.openFileMenu)
+        fileMenu.add_command(label="Salir", command=self.onExit)
+        menubar.add_cascade(label="Archivo", menu=fileMenu)
 
-        # Load an image using OpenCV
-        cv_img = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+        # Create canvas and put image on it
+        self.canvas = tk.Canvas(self.master, highlightthickness=0,
+                                xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+        self.canvas.grid(row=0, column=0, sticky='nswe')
+        self.canvas.update()  # wait till canvas is created
+        vbar.configure(command=self.scroll_y)  # bind scrollbars to the canvas
+        hbar.configure(command=self.scroll_x)
+        # Make the canvas expandable
+        self.master.rowconfigure(0, weight=1)
+        self.master.columnconfigure(0, weight=1)
+        self.bindCanvasEvents()
+        self.image = Image.open(self.filename)  # open image
+        self.width, self.height = self.image.size
+        self.imscale = 1.0  # scale for the canvaas image
+        self.delta = 1.3  # zoom magnitude
+        # Put image into container rectangle and use it to set proper coordinates to the image
+        self.container = self.canvas.create_rectangle(0, 0, self.width, self.height, width=0)
 
-        # Get the image dimensions (OpenCV stores image data as NumPy ndarray)
-        height, width, no_channels = cv_img.shape
+        self.show_image()
 
-        # Create a canvas that can fit the above image
-        canvas = ResizingCanvas(self.window, width=width, height=height)
-        canvas.pack(fill="both", expand=True)
+    def bindCanvasEvents(self):
+        """ Bind events to the Canvas """
+        self.canvas.bind('<Configure>', self.show_image)  # canvas is resized
+        self.canvas.bind('<ButtonPress-1>', self.move_from)
+        self.canvas.bind('<B1-Motion>', self.move_to)
+        self.canvas.bind('<MouseWheel>', self.wheel)  # with Windows and MacOS, but not Linux
+        self.canvas.bind('<Button-5>', self.wheel)  # only with Linux, wheel scroll down
+        self.canvas.bind('<Button-4>', self.wheel)  # only with Linux, wheel scroll up
 
-        # Use PIL (Pillow) to convert the NumPy ndarray to a PhotoImage
-        photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
+    def scroll_y(self, *args, **kwargs):
+        """ Scroll canvas vertically and redraw the image """
+        self.canvas.yview(*args, **kwargs)  # scroll vertically
+        self.show_image()  # redraw the image
 
-        # Add a PhotoImage to the Canvas
-        canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
+    def scroll_x(self, *args, **kwargs):
+        """ Scroll canvas horizontally and redraw the image """
+        self.canvas.xview(*args, **kwargs)  # scroll horizontally
+        self.show_image()  # redraw the image
 
-        self.window.mainloop()
+    def move_from(self, event):
+        """ Remember previous coordinates for scrolling with the mouse """
+        self.canvas.scan_mark(event.x, event.y)
+
+    def move_to(self, event):
+        """ Drag (move) canvas to the new position """
+        self.canvas.scan_dragto(event.x, event.y, gain=1)
+        self.show_image()  # redraw the image
+
+    def wheel(self, event):
+        """ Zoom with mouse wheel """
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        bbox = self.canvas.bbox(self.container)  # get image area
+        if bbox[0] < x < bbox[2] and bbox[1] < y < bbox[3]:
+            pass  # Ok! Inside the image
+        else:
+            return  # zoom only inside image area
+        scale = 1.0
+        # Respond to Linux (event.num) or Windows (event.delta) wheel event
+        if event.num == 5 or event.delta == -120:  # scroll down
+            i = min(self.width, self.height)
+            if int(i * self.imscale) < 30: return  # image is less than 30 pixels
+            self.imscale /= self.delta
+            scale /= self.delta
+        if event.num == 4 or event.delta == 120:  # scroll up
+            i = min(self.canvas.winfo_width(), self.canvas.winfo_height())
+            if i < self.imscale: return  # 1 pixel is bigger than the visible area
+            self.imscale *= self.delta
+            scale *= self.delta
+        self.canvas.scale('all', x, y, scale, scale)  # rescale all canvas objects
+        self.show_image()
+
+    def show_image(self, event=None):
+        """ Show image on the Canvas """
+        bbox1 = self.canvas.bbox(self.container)  # get image area
+        # Remove 1 pixel shift at the sides of the bbox1
+        bbox1 = (bbox1[0] + 1, bbox1[1] + 1, bbox1[2] - 1, bbox1[3] - 1)
+        bbox2 = (self.canvas.canvasx(0),  # get visible area of the canvas
+                 self.canvas.canvasy(0),
+                 self.canvas.canvasx(self.canvas.winfo_width()),
+                 self.canvas.canvasy(self.canvas.winfo_height()))
+        bbox = [min(bbox1[0], bbox2[0]), min(bbox1[1], bbox2[1]),  # get scroll region box
+                max(bbox1[2], bbox2[2]), max(bbox1[3], bbox2[3])]
+        if bbox[0] == bbox2[0] and bbox[2] == bbox2[2]:  # whole image in the visible area
+            bbox[0] = bbox1[0]
+            bbox[2] = bbox1[2]
+        if bbox[1] == bbox2[1] and bbox[3] == bbox2[3]:  # whole image in the visible area
+            bbox[1] = bbox1[1]
+            bbox[3] = bbox1[3]
+        self.canvas.configure(scrollregion=bbox)  # set scroll region
+        x1 = max(bbox2[0] - bbox1[0], 0)  # get coordinates (x1,y1,x2,y2) of the image tile
+        y1 = max(bbox2[1] - bbox1[1], 0)
+        x2 = min(bbox2[2], bbox1[2]) - bbox1[0]
+        y2 = min(bbox2[3], bbox1[3]) - bbox1[1]
+        if int(x2 - x1) > 0 and int(y2 - y1) > 0:  # show image if it in the visible area
+            x = min(int(x2 / self.imscale), self.width)  # sometimes it is larger on 1 pixel...
+            y = min(int(y2 / self.imscale), self.height)  # ...and sometimes not
+            image = self.image.crop((int(x1 / self.imscale), int(y1 / self.imscale), x, y))
+            imagetk = ImageTk.PhotoImage(image.resize((int(x2 - x1), int(y2 - y1))))
+            imageid = self.canvas.create_image(max(bbox2[0], bbox1[0]), max(bbox2[1], bbox1[1]),
+                                               anchor='nw', image=imagetk)
+            self.canvas.lower(imageid)  # set image into background
+            self.canvas.imagetk = imagetk  # keep an extra reference to prevent garbage-collection
 
     def onExit(self):
         if messagebox.askokcancel("Salir", "¿De seguro que quieres salir?"):
-            self.window.destroy()
+            self.master.destroy()
             sys.exit()
+
+    def openFileMenu(self):
+        file = fd.askopenfilename()
+        if file:
+            self.filename = file
+            self.image = Image.open(self.filename)  # open image
+            self.width, self.height = self.image.size
 
 
 def main():
     root = Tk()
     root.title('Segmentación de venas')
     root.geometry("1000x500")
-    app = App(root)
+    app = App(root, )
     app.mainloop()
 
 
