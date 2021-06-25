8df8530207de2a3a80d5eaf3442395b826226a00
Néstor Ojeda González <nestorojedagonzalez@gmail.com>
2021-06-25 20:43:31 +0100

    Actualización del gitignore y script de prueba de la esqueletonización

---
+++ scripts/skel_script.py
@@ -0,0 +1,30 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from VeinSegmentation.Enhance import enhance_medical_image, segmentation
+from VeinSegmentation.Skeletonization import skeletonization
+
+scaleX = 1
+scaleY = 1
+
+y = 1300  # donde empieza el corte en y
+x = 1600  # donde empieza el corte en x
+h = 600  # tamaño del corte en h
+w = 600  # tamaño del corte en y
+
+iters = 2
+threshold = 4.5
+order = 2
+
+original = (cv2.imread('../imagenes_orginales/Caso A BN.png'))
+zoom = original[y:y + h, x:x + w]
+plt.imshow(zoom, cmap='gray')
+plt.title('Original')
+plt.show()
+
+enhanced = enhance_medical_image(zoom.copy())
+
+old = skeletonization(enhanced.copy())
+plt.imshow(old, cmap='gray')
+plt.title('Old Skeletonization')
+plt.show()
